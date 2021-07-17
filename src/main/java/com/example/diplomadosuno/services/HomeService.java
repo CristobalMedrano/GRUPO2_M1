0c0001c25ae0dd341cd9bce37da1d4cb895776d8
Cristobal Medrano <cristobal.medrano@usach.cl>
2021-07-17 17:34:52 -0400

    CrossOrigin para el front

--- src/main/java/com/example/diplomadosuno/services/HomeService.java
+++ src/main/java/com/example/diplomadosuno/services/HomeService.java
@@ -1,11 +1,13 @@
 package com.example.diplomadosuno.services;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@CrossOrigin("*")
 public class HomeService {
 
     @GetMapping("/")
